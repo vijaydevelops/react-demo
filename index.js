75935d4be92286786fadd27620e90ef478c4808a
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-02-27 12:25:14 +0530

    Exercise 1
    Basic react app with jsx using CDN links

---
+++ index.js
@@ -0,0 +1,11 @@
+const page = (
+    <nav>
+        <h1>Header BrandCom</h1>
+        <p>Described here...</p>
+    </nav>
+);
+// jsx element should have children under a single parent node
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+);
