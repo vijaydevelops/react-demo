de66d3969315a04f569deebfc689cad2ac82840a
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-05-21 21:56:03 +0530

    changes 2022-05-21
    - React Router DOM added in package list using yarn
    - index.js content moved to demo1.js
    - index.js changed to Router container
    - Demo for useContext added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,71 @@
-import React from "react";  // for recognizing jsx
-import ReactDOM from "react-dom";
+import React, { useState, Component } from "react";
+import ReactDOM, { render } from "react-dom";
+import {
+    BrowserRouter as Router,
+    Switch,     // Routes in v6
+    Route,
+    Link,
+    Redirect    // Navigate in v6
+} from "react-router-dom";
+import ContextDemo from "./useContextDemo";
+import demo1 from "./demo1";
 
-const navbar = (
-    <nav>
-        <h1>BrandCom</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
-);
+function App() {
+    const [user, setUser] = useState({
+        name: "Demo",
+        isUserAuthenticated: true
+    });
+
+    // render() function
+    return (
+        <div>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/context-demo">Context Demo</Link>
+                        </li>
+                        <li>
+                            <Link to="/current-demo">Demo 1</Link>
+                        </li>
+                    </ul>
+                    <hr />
+                    <br />
+                    <br />
+
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            component={_ => (
+                                <>
+                                    {user.isUserAuthenticated
+                                        ? <Redirect to="/current-demo" />
+                                        : <Redirect to="/context-demo" />
+                                    }
+                                </>
+                            )
+                            }
+                        />
+                        <Route
+                            exact
+                            path="/context-demo"
+                            // component={_ => <ContextDemo />}
+                            component={ContextDemo}
+                        />
+                        <Route
+                            exact
+                            path="/current-demo"
+                            component={_ => demo1}
+                        />
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
+}
 
 ReactDOM.render(
-    navbar,
+    <App />,
     document.getElementById("root")
 );
