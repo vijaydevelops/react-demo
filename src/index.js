bb7f43cc5cb2ba70430c29050674427103792a4a
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-05-21 21:26:59 +0530

    changes 2022-05-21
    - react updated to v18
    - setting up react-router-dom (for react-18)
    - existing demo (index.js) changed to demo1.js
    - demo added for useContext, createContext
    - App component created in index,js with Router and Routes (for react 18)
    
    - Appsample - reference added from https://www.pluralsight.com/guides/how-to-set-react-router-default-route-redirect-to-home

--- src/index.js
+++ src/index.js
@@ -1,25 +1,83 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, Component } from "react";
+import ReactDOM, { render } from "react-dom";
+import { createRoot } from "react-dom/client";
+import {
+    BrowserRouter as Router,
+    Routes,     // Switch,
+    Route,
+    Link,
+    Navigate    // Redirect
+} from "react-router-dom";
+import ContextDemo from "./useContextDemo";
+import demo1 from "./demo1";
 
-const Navbar = () => (
-    <nav>
-        <img src="/images/react-logo.png" alt="react logo" width="40px" />
-    </nav>
-);
+function App() {
+    const [user, setUser] = useState({
+        name: "Demo",
+        isUserAuthenticated: true
+    });
 
-const block = (<div>
-    <Navbar />
-    <h1>Fun Facts about React</h1>
-    <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterpise apps, including mobile apps</li>
-    </ul>
-</div>)
+    // render() function
+    return (
+        <div>
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/context-demo">Context Demo</Link>
+                        </li>
+                        <li>
+                            <Link to="/current-demo">Demo 1</Link>
+                        </li>
+                    </ul>
+                    <hr />
+                    <br />
+                    <br />
 
+                    <Routes>
+                        <Route
+                            exact
+                            path="/"
+                            element={
+                                <>
+                                    {
+                                        user.isUserAuthenticated ?
+                                            <Navigate to="/current-demo" /> :
+                                            <Navigate to="/context-demo" />
+                                    }
+                                </>
+                            }
+                        />
+
+                        {/*<Route exact path="/contextdemo" component={ContextDemo} /> */}
+                        <Route
+                            exact
+                            path="/context-demo"
+                            element={<ContextDemo />}
+                        />
+                        <Route
+                            exact
+                            path="/current-demo"
+                            element={demo1}
+                        />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
+}
+
+
+const root = createRoot(document.getElementById('root'));
+// Warning: You are importing createRoot from "react-dom" which is not supported. You should instead import it from "react-dom/client".
+// Uncaught TypeError: react_dom_client__WEBPACK_IMPORTED_MODULE_2___default(...) is not a function at Module../src/index.js
+root.render(<App />);
+
+/*
 ReactDOM.render(
-    block,
+    <App />,
     document.getElementById("root")
 );
+// Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17.
+*/
+
