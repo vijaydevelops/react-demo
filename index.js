05835cfc1b1dc9d47bdac13d6b2bc1de7bf1a217
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-09-26 15:55:59 +0530

    Exercise Ch1

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-// imperative
-// usually avoided in REACT
-/*
-const h1 = document.createElement("h1");
-h1.textContent = "Hi there";
-document.getElementById("root").append(h1)
-// console.log(h1)
-*/
-
-/* composable - big part built from or composed from small chunks */
-const MainContent = function () {
-    return <p>This is main content</p>
-}
+const OtherNav = (
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+)
 
 const page = (
     <nav>
         <h1>Header BrandCom</h1>
-        <p>Described here...</p>
-        <MainContent />
+        <OtherNav />
     </nav>
 );
-// console.log(page)  POJO, not DOM element
-// JS XML
-// declarative
-// jsx element should have children under a single parent node
+
 ReactDOM.render(
     navbar,
     document.getElementById("root")
